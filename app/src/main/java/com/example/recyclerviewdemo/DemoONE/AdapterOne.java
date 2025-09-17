5f78862327bc5fa4830c75d2dad9cbcd671e037b
Hadiya Saleem <hadiyasaleem36@gmail.com>
2025-09-17 04:26:42 -0700

    RecyclerView

---
+++ app/src/main/java/com/example/recyclerviewdemo/DemoONE/AdapterOne.java
@@ -0,0 +1,55 @@
+package com.example.recyclerviewdemo.DemoONE;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerviewdemo.R;
+
+import java.util.Arrays;
+
+public class AdapterOne extends RecyclerView.Adapter<AdapterOne.MyViewHolder>
+{
+    int[] arr_img;
+    String[] arr_names;
+    public AdapterOne(int[]arr_img ,String[] arr_names){
+        this.arr_img=arr_img;
+        this.arr_names=arr_names;
+    }
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater=LayoutInflater.from(parent.getContext());
+        View view=inflater.inflate(R.layout.recyclerview_layout, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.img.setImageResource(arr_img[position]);
+        holder.names.setText(arr_names[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView img;
+        private TextView names;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            img=(ImageView) itemView.findViewById(R.id.image_one);
+            names=(TextView) itemView.findViewById(R.id.tv_one);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewdemo/DemoONE/RecyclerViewDemo.java
@@ -0,0 +1,35 @@
+package com.example.recyclerviewdemo.DemoONE;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recyclerviewdemo.R;
+
+public class RecyclerViewDemo extends AppCompatActivity {
+    RecyclerView recyclerview_one;
+    String[] names={"Hadia","Laraib","Sharfa","Aliyan"};
+    int[] img={R.drawable.hijab_jpg,R.drawable.cloud,R.drawable.nature,R.drawable.mountain};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_recycler_view_demo);
+
+        recyclerview_one=(RecyclerView) findViewById(R.id.recyclerview_one);
+        AdapterOne adapterOne=new AdapterOne(img,names);
+
+        recyclerview_one.setLayoutManager(new LinearLayoutManager(this));
+        recyclerview_one.setAdapter(adapterOne);
+
+
+    }
+}
